6796f74ed574f68ef7f5eeec1b6bdef476d98738
Nellie K <noircir@users.noreply.github.com>
2019-01-09 20:10:32 -0500

    more documentation

--- lib/cards.ex
+++ lib/cards.ex
@@ -10,6 +10,7 @@ defmodule Cards do
     values = ["Ace", "Two", "Three", "Four", "Five"]
     suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
+    # One way: a loop of loops, then flattening.
     # cards = for value <- values do
     #   for suit <- suits do
     #     "#{value} of #{suit}"
@@ -27,6 +28,16 @@ defmodule Cards do
     Enum.shuffle(deck)
   end
 
+  @doc """
+    Determines whether a deck contains a given card.
+
+  ## Examples
+
+      iex> d = Cards.create_deck
+      iex> Cards.contains?(d, "Ace of Hearts")
+      true
+
+  """
   def contains?(deck, card) do
     Enum.member?(deck, card)
   end
@@ -41,6 +52,7 @@ defmodule Cards do
       iex> {hand, deck} = Cards.deal(deck, 1)
       iex> hand
       ["Ace of Spades"]
+
   """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
--- test/cards_test.exs
+++ test/cards_test.exs
@@ -2,7 +2,16 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
+  test "create_deck makes 20 cards" do
+    deck_length = length(Cards.create_deck)
+    assert deck_length == 20
   end
+
+  test "shuffling a deck retains the same amount of cards and randomizes the deck" do
+    deck = Cards.create_deck
+    shuffled_deck = Cards.shuffle(deck)
+    assert length(shuffled_deck) == 20
+    refute deck == shuffled_deck
+  end
+
 end
